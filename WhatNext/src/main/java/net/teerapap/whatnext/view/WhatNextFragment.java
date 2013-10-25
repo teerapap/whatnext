884b429dfef2ba4aa6f83d53a9ab124785f4f727
Teerapap Changwichukarn <teerapap.c@gmail.com>
2013-10-26 02:20:49 +0700

    Implement simple NextTaskListener to plug with TaskService

--- WhatNext/src/main/java/net/teerapap/whatnext/view/WhatNextFragment.java
+++ WhatNext/src/main/java/net/teerapap/whatnext/view/WhatNextFragment.java
@@ -3,25 +3,43 @@ package net.teerapap.whatnext.view;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import net.teerapap.whatnext.R;
+import net.teerapap.whatnext.model.Task;
+import net.teerapap.whatnext.model.WhenCondition;
+import net.teerapap.whatnext.service.NextTaskListener;
+import net.teerapap.whatnext.service.TaskService;
 
 /**
  * Fragment for WhatNext page. It is the main fragment for the application.
  * Created by teerapap on 10/21/13.
  */
-public class WhatNextFragment extends Fragment {
+public class WhatNextFragment extends Fragment implements NextTaskListener {
+
+    private static String TAG = "WhatnNextFragment";
 
     private Button mClearBtn;
+    private Button mNextTaskBtn;
+    private TextView mTaskTitleText;
+    // TODO: Refactor this toggle buttons to a class.
     private ToggleButton mWhenHomeToggleBtn;
     private ToggleButton mWhenFreeToggleBtn;
     private ToggleButton mWhenWorkToggleBtn;
     private ToggleButton mWhenShoppingToggleBtn;
+
+    private TaskService mTaskService;
+
+    public WhatNextFragment() {
+        mTaskService = TaskService.getInstance();
+    }
 
     /**
      * Static method to initialize the fragment
@@ -52,6 +70,8 @@ public class WhatNextFragment extends Fragment {
     private void initMemberViews() {
         Activity activity = getActivity();
         mClearBtn = (Button) activity.findViewById(R.id.clear_btn);
+        mNextTaskBtn = (Button) activity.findViewById(R.id.next_btn);
+        mTaskTitleText = (TextView) activity.findViewById(R.id.task_title);
         mWhenHomeToggleBtn = (ToggleButton) activity.findViewById(R.id.when_home_toggle);
         mWhenFreeToggleBtn = (ToggleButton) activity.findViewById(R.id.when_free_toggle);
         mWhenWorkToggleBtn = (ToggleButton) activity.findViewById(R.id.when_work_toggle);
@@ -62,12 +82,22 @@ public class WhatNextFragment extends Fragment {
      * Setup proper event listeners inside fragment
      */
     private void setUpEventListeners() {
+        // Subscribe for next task
+        mTaskService.setNextTaskListener(this);
+
         mClearBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resetWhenToggleBtns();
             }
         });
+        mNextTaskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextTask();
+            }
+        });
+
     }
 
     /**
@@ -78,5 +108,38 @@ public class WhatNextFragment extends Fragment {
         mWhenFreeToggleBtn.setChecked(false);
         mWhenWorkToggleBtn.setChecked(false);
         mWhenShoppingToggleBtn.setChecked(false);
+    }
+
+    private void nextTask() {
+
+        // Construct when condition
+        WhenCondition when = new WhenCondition()
+                .atHome(mWhenHomeToggleBtn.isChecked())
+                .freeTime(mWhenFreeToggleBtn.isChecked())
+                .atWork(mWhenWorkToggleBtn.isChecked())
+                .shopping(mWhenShoppingToggleBtn.isChecked());
+
+        // Request for next task
+        mTaskService.requestNextTask(when);
+    }
+
+    @Override
+    public void onNextTask(Task task) {
+        // TODO: Do on UI thread.
+        mTaskTitleText.setText(task.getTitle());
+    }
+
+    @Override
+    public void onError(Error error) {
+        Log.e(TAG, "Error on next task", error);
+        Toast.makeText(getActivity().getApplicationContext(), "Error occurred", Toast.LENGTH_SHORT)
+             .show();
+    }
+
+    @Override
+    public void onNoMoreTask() {
+        // TODO: Implement this properly and do on UI thread.
+        Toast.makeText(getActivity().getApplicationContext(), "No more task.", Toast.LENGTH_SHORT)
+             .show();
     }
 }
